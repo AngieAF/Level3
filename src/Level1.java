0e4a86a89596f3772eae176bf1f5f76a48081e2e
Angie Akhmatshina <a.angie.f@gmail.com>
2021-04-09 10:18:23 -0400

    add Level1.java

---
+++ src/Level1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Level1
+{
+    public static int squirrel(int N)
+    {
+        double total = 1;
+        for(int i = 1; i <= N; i++)
+            total = i * total;
+
+        String str = String.valueOf(total);
+
+        int diamond = Integer.parseInt(String.valueOf(str.charAt(0)));
+
+        return diamond;
+
+    }
+}
