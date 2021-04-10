3648e2113b7daa957a8a4c5aca11746399ee4e6b
Angie Akhmatshina <a.angie.f@gmail.com>
2021-04-10 10:51:37 -0400

    update Level1.java

--- src/Level1.java
+++ src/Level1.java
@@ -2,17 +2,31 @@ import java.util.*;
 
 public class Level1
 {
-    public static int squirrel(int N)
+    public static int odometer(int [] oksana)
     {
-        double total = 1;
-        for(int i = 1; i <= N; i++)
-            total = i * total;
+        int[] speed = new int[oksana.length / 2];
+        int[] time = new int[oksana.length / 2];
 
-        String str = String.valueOf(total);
+        int m = 0, n = 0;
+        for(int i = 0; i < oksana.length; i++) {
+            if(i % 2 == 0) {
+                speed[m] = oksana[i];
+                m++;
+            }
+            else {
+                time[n] = oksana[i];
+                n++;
+            }
+        }
 
-        int diamond = Integer.parseInt(String.valueOf(str.charAt(0)));
+        for(int k = 0; k < time.length - 1; k++)
+            time[k + 1] = time[k + 1] - time[k];
 
-        return diamond;
+        int total = 0;
+        for(int j = 0; j < speed.length; j++)
+            total += speed[j] * time[j];
+
+        return total;
 
     }
 }
