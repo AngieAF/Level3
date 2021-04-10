1b6b54fb7fb33adc017d297e340f33185af1f2f3
Angie Akhmatshina <a.angie.f@gmail.com>
2021-04-10 13:53:34 -0400

    update Level1.java

--- src/Level1.java
+++ src/Level1.java
@@ -2,31 +2,16 @@ import java.util.*;
 
 public class Level1
 {
-    public static int odometer(int [] oksana)
-    {
-        int[] speed = new int[oksana.length / 2];
-        int[] time = new int[oksana.length / 2];
+    public static int ConquestCampaign(int N, int M, int L, int [] battalion) {
 
-        int m = 0, n = 0;
-        for(int i = 0; i < oksana.length; i++) {
-            if(i % 2 == 0) {
-                speed[m] = oksana[i];
-                m++;
-            }
-            else {
-                time[n] = oksana[i];
-                n++;
-            }
+        int total_square = N * M;
+
+        int filled = 0, i = 1;
+        while(filled < total_square) {
+            filled += L * i;
+            System.out.println(filled);
+            i++;
         }
-
-        for(int k = time.length - 1; k > 0; k--)
-            time[k] -= time[k-1];
-
-        int total = 0;
-        for(int j = 0; j < speed.length; j++)
-            total += speed[j] * time[j];
-
-        return total;
-
+        return (i-1);
     }
 }
