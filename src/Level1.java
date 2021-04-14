74a1ff2a76191c4371b00c88e9bd5891aa77fd71
Angie Akhmatshina <a.angie.f@gmail.com>
2021-04-13 20:26:42 -0400

    update Level1.java

--- src/Level1.java
+++ src/Level1.java
@@ -2,37 +2,39 @@ import java.util.*;
 
 public class Level1
 {
-    public static int [] SynchronizingTables(int N, int [] ids, int [] salary) {
+        public static String PatternUnlock(int N, int [] hits) {
 
-        int[] temp_arr = new int[N];
-        for(int i = 0; i < ids.length; i++)
-            temp_arr[i] = ids[i];
-
-        sorting(temp_arr);
-        sorting(salary);
-
-        HashMap<String, String> salaryTable = new HashMap<String, String>();
-        for(int i = 0; i < ids.length; i++)
-            salaryTable.put(String.valueOf(temp_arr[i]), String.valueOf(salary[i]));
-
-        for(int i = 0; i < ids.length; i++)
-            salary[i] = Integer.parseInt(salaryTable.get(String.valueOf(ids[i])));
-
-        return salary;
-    }
-
-    public static int[] sorting(int[] arr) {
-        for(int j = 0; j <= arr.length/2 + 1; j++) {
-            int max = arr[0];
-            for(int i = 0; i < arr.length - 1 - j; i++) {
-                if(arr[i] >= arr[i + 1]) {
-                    max = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = max;
-                }
+            double length = 0.0;
+            for(int i = 0; i < hits.length - 1; i++) {
+                if((hits[i] != (N - 1)) && (hits[i] == 6 && hits[i + 1] == 2) || (hits[i] == 2 && hits[i + 1] == 6))
+                    length += 1.41421;
+                else if((hits[i] != (N - 1)) && (hits[i] == 9 && hits[i + 1] == 2) || (hits[i] == 2 && hits[i + 1] == 9))
+                    length += 1.41421;
+                else if((hits[i] != (N - 1)) && (hits[i] == 4 && hits[i + 1] == 2) || (hits[i] == 2 && hits[i + 1] == 4))
+                    length += 1.41421;
+                else if((hits[i] != (N - 1)) && (hits[i] == 7 && hits[i + 1] == 2) || (hits[i] == 2 && hits[i + 1] == 7))
+                    length += 1.41421;
+                else length += 1;
             }
+
+            String str = String.valueOf(length);
+
+            ArrayList<Character> fin = new ArrayList<>();
+
+            for(int j = 0; j < str.length(); j++) {
+                if(str.charAt(j) == '.' || str.charAt(j) == '0')
+                    continue;
+                else fin.add(str.charAt(j));
+            }
+
+            String arr[] = new String[fin.size()];
+            
+            for(int j =0; j < fin.size(); j++)
+                arr[j] = String.valueOf(fin.get(j));
+
+            str = String.join("", arr);
+
+            return str;
         }
-        return arr;
-    }
 
 }
