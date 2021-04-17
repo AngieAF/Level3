b88a0812dc0252264537cc4f735ff0dec3b8b97d
Angie Akhmatshina <a.angie.f@gmail.com>
2021-04-17 16:08:56 -0400

    add updated Level1

--- src/Level1.java
+++ src/Level1.java
@@ -4,27 +4,35 @@ public class Level1
 {
     public static int [] WordSearch(int len, String s, String subs) {
         ArrayList<Character> fin = new ArrayList<>();
-        int end = len - 1;
+        int end = len;
         int begin = 0;
         int counter = 0;
         int sub_length = end - begin;
+        boolean flag = false;
 
         if(len > s.length())
             len = s.length();
-        
+
         while(counter < s.length()) {
-            if(end + len > s.length()) {
+            if(end >= s.length()) {
                 end = s.length() - 1;
             }
 
             for(int i = end; i >= begin; i--){
                 if(s.charAt(i) == ' ') {
                     end = i + 1;
+                    flag = true;
                     break;
                 }
             }
-            for(int j = begin; j < end; j++)
+
+            /*if(flag == false && s.length() - counter > len) end = len;
+            else end = s.length();*/
+
+            for(int j = begin; j < end; j++) {
                 fin.add(s.charAt(j));
+                System.out.println(fin);
+            }
             fin.add('\n');
 
             counter += sub_length;
@@ -32,13 +40,11 @@ public class Level1
             begin = end;
             end += len;
 
-            if(end + len > s.length()) {
-                end = s.length() - 1;
-            }
-
         }
 
+        System.out.println(fin);
         ArrayList<Integer> res = new ArrayList<>();
+
 
         ArrayList<Character> line = new ArrayList<>();
         for(int i = 0; i < fin.size(); i++){
@@ -52,22 +58,28 @@ public class Level1
                     result.append(c);
                 }
                 String output = result.toString();
-
+                System.out.println(output);
                 String subs1 = subs + " ";
                 String subs2 = " " + subs + " ";
                 String subs3 = " " + subs + "\n";
+                String subs4 = subs + "\n";
 
-                if(output.contains(subs1) || output.contains(subs2) || output.contains(subs3)){
+                if(output.contains(subs1) || output.contains(subs2) || output.contains(subs3) || output.contains(subs4) || output.equals(subs)){
                     res.add(1);
                 } else res.add(0);
                 line.clear();
             }
         }
 
+        System.out.println(res);
+
         int [] result = new int [res.size()];
-        for(int j =0; j < res.size(); j++){
+        for(int j = 0; j < res.size(); j++){
             result[j] = res.get(j);
         }
+
+        for(int i = 0; i < result.length; i++)
+            System.out.print(result[i]);
 
         return result;
 
