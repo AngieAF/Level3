98f44b67453e03390a59ca1205e04ae34a38a478
Angie Akhmatshina <a.angie.f@gmail.com>
2021-05-05 12:50:18 -0400

    update Level1 file

--- src/Level1.java
+++ src/Level1.java
@@ -4,29 +4,29 @@ public class Level1
 {
     public static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2) {
         boolean flag = false;
-        int j = 0, counter = 0, sp = 0;
+        int j = 0, counter = 0, old_pos = 0;
         for (int i = 0; i < S1.length(); i++) {
-            if(counter == W1) break;
+            if(counter == (S2.length() - 1)) break;
 
-            if (S1.charAt(i) == ' ') sp++;
-            else {
-                if (S2.charAt(j) == ' ' && flag == true) {
-                    j++;
-                    i = i + j - sp;
-                }
-                if (S2.charAt(j) == S1.charAt(i)) {
-                    j++;
-                    flag = true;
-                    counter++;
-                } else {
-                    flag = false;
-                    i = i - counter - sp;
-                    j = 0;
-                    counter = 0;
-                    sp = 0;
-                }
+            if (S2.charAt(j) == ' ' && flag == true) {
+                i = i + (W1 - W2 + 1);
+                j++;
             }
-
+            if (S2.charAt(j) == S1.charAt(i)) {
+                j++;
+                flag = true;
+                counter++;
+                if(counter % W2 == 0)
+                    old_pos = i;
+            } else {
+                if(flag == true) {
+                    i = old_pos;
+                }
+                flag = false;
+                old_pos = 0;
+                j = 0;
+                counter = 0;
+            }
         }
         return flag;
     }
