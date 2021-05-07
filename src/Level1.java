995dbae2064777f8e5da555dc34c1643d03a7f6e
Angie Akhmatshina <a.angie.f@gmail.com>
2021-05-07 12:24:31 -0400

    update Level1 file

--- src/Level1.java
+++ src/Level1.java
@@ -6,27 +6,27 @@ public class Level1
         boolean flag = false;
         int j = 0, counter = 0, old_pos = 0;
         for (int i = 0; i < S1.length(); i++) {
-            if(counter == W2 * H2) break;
+            if(counter == H2 * W2) break;
 
             if (S2.charAt(j) == ' ' && flag == true) {
                 i = i + (W1 - W2 + 1);
                 j++;
             }
             if (S2.charAt(j) == S1.charAt(i)) {
-                j++;
+                j++; counter++;
                 flag = true;
-                counter++;
-                if(counter <= W2)
+
+                if(counter == 1)
                     old_pos = i;
             } else {
                 if(flag == true)
                     i = old_pos;
+
                 flag = false;
-                old_pos = 0;
-                j = 0;
-                counter = 0;
+                old_pos = 0;  j = 0; counter = 0;
             }
         }
+        if(counter != H2 * W2) flag = false;
         return flag;
     }
 }
