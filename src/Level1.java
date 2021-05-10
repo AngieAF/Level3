6f4baf5c6afc8a9eef6a60717c7c2a7b33b1ceab
Angie Akhmatshina <a.angie.f@gmail.com>
2021-05-10 13:52:42 -0400

    update Level1 file

--- src/Level1.java
+++ src/Level1.java
@@ -2,31 +2,32 @@ import java.util.*;
 
 public class Level1
 {
-    public static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2) {
-        boolean flag = false;
-        int j = 0, counter = 0, old_pos = 0;
-        for (int i = 0; i < S1.length(); i++) {
-            if(counter == H2 * W2) break;
-
-            if (S2.charAt(j) == ' ' && flag == true) {
-                i = i + (W1 - W2 + 1);
-                j++;
-            }
-            if (S2.charAt(j) == S1.charAt(i)) {
-                j++; counter++;
-                flag = true;
-
-                if(counter == 1)
-                    old_pos = i;
-            } else {
-                if(flag == true)
-                    i = old_pos;
-
-                flag = false;
-                old_pos = 0;  j = 0; counter = 0;
+    public static int MaximumDiscount(int N, int [] price) {
+        int max = price[0];
+        for(int j = 0; j <= N/2 + 2; j++) {
+            for(int i = 0; i < N - 1 - j; i++) {
+                if(price[i] >= price[i + 1]) {
+                    max = price[i];
+                    price[i] = price[i + 1];
+                    price[i + 1] = max;
+                }
             }
         }
-        if(counter != H2 * W2) flag = false;
-        return flag;
+
+        int temp = 0;
+        for(int i = 0; i <= N/2; i++) {
+            temp = price[i];
+            price[i] = price[N - 1 - i];
+            price[N - 1 - i] = temp;
+        }
+
+        int total = 0, sum = 0;
+        for(int i = 0; i < N; i++) {
+            sum += price[i];
+            if((i + 1) % 3 != 0)
+                total += price[i];
+        }
+
+        return (sum - total);
     }
 }
