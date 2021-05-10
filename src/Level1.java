41a1944068a0274946ac1f185510e512e7888c86
Angie Akhmatshina <a.angie.f@gmail.com>
2021-05-10 18:53:16 -0400

    update Level1 file

--- src/Level1.java
+++ src/Level1.java
@@ -3,22 +3,25 @@ import java.util.*;
 public class Level1
 {
     public static int MaximumDiscount(int N, int [] price) {
-        int max = price[0];
-        for(int j = 0; j <= N/2 + 2; j++) {
-            for(int i = 0; i < N - 1 - j; i++) {
-                if(price[i] >= price[i + 1]) {
-                    max = price[i];
+        boolean sorted = false;
+        int temp = 0;
+        while(!sorted) {
+            sorted = true;
+            for (int i = 0; i < price.length - 1; i++) {
+                if (price[i] >= price[i + 1]) {
+                    temp = price[i];
                     price[i] = price[i + 1];
-                    price[i + 1] = max;
+                    price[i + 1] = temp;
+                    sorted = false;
                 }
             }
         }
 
-        int temp = 0;
-        for(int i = 0; i <= N/2; i++) {
-            temp = price[i];
-            price[i] = price[N - 1 - i];
-            price[N - 1 - i] = temp;
+        int t = 0;
+        for(int i = N - 1; i >= N/2 ; i--) {
+            t = price[price.length - 1 - i];
+            price[price.length - 1 - i] = price[i];
+            price[i] = t;
         }
 
         int total = 0, sum = 0;
