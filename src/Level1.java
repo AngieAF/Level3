22e576079e99ef858db7ec41dbcdf70a2109a855
Angie Akhmatshina <a.angie.f@gmail.com>
2021-05-13 15:54:34 -0400

    update Level1 file

--- src/Level1.java
+++ src/Level1.java
@@ -2,36 +2,52 @@ import java.util.*;
 
 public class Level1
 {
-    public static boolean MisterRobot(int N, int[] data) {
-        int index = 0;
-        for (int i = N - 1; i >= 0; i--) {
-            if (data[i] != (i - 1)) {
-                index = (i - 1);
-                break;
-            }
-        }
+    public static String [] ShopOLAP(int N, String [] items) {
+        HashMap <String, String> soldGoods = new HashMap<>();
 
         int temp = 0;
-        while (index >= index - 2) {
-            for (int j = index; j > index - 2; j--) {
-                if (data[j] < data[j - 1]) {
-                    temp = data[j - 1];
-                    data[j - 1] = data[j];
-                    data[j] = temp;
-                }
+        for(int i = 0; i < items.length - 1; i += 2) {
+            if(soldGoods.containsKey(items[i])) {
+                temp = Integer.parseInt(soldGoods.get(items[i])) + Integer.parseInt(items[i + 1]);
+                soldGoods.put(items[i], String.valueOf(temp));
             }
-            index -= 2;
-            if(index == 1) break;
+            else soldGoods.put(items[i], items[i + 1]);
         }
 
-        boolean flag = false;
-        for (int k = 0; k < N; k++) {
-            if (data[k] != k + 1) {
-                flag = false;
-                break;
-            }
-            else flag = true;
+        String [] fin = new String[soldGoods.size() * 2];
+
+        int i = 0;
+
+        for (String key : soldGoods.keySet()) {
+            fin[i] = key;
+            i += 2;
+            if(i == (soldGoods.size() * 2 - 1)) break;
         }
-        return flag;
+
+        int j = 1;
+        for (String value : soldGoods.values()) {
+            fin[j] = value;
+            j += 2;
+            if(j == (soldGoods.size() * 2)) break;
+        }
+
+        String num = "", item = "";
+        boolean sorted = false;
+
+        while(!sorted) {
+            for(int k = 1; k < fin.length - 2; k += 2) {
+                sorted = true;
+                if(Integer.parseInt(fin[k]) > Integer.parseInt(fin[k + 2])) {
+                    num = fin[k];
+                    item = fin[k - 1];
+                    fin[k] = fin[k + 2];
+                    fin[k - 1] = fin[k + 1];
+                    fin[k + 2] = num;
+                    fin[k + 1] = item;
+                    sorted = false;
+                }
+            }
+        }
+        return fin;
     }
 }
