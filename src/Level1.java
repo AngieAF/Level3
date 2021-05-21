e804bc915b42b285b8fb685dc18458561793e83f
Angie Akhmatshina <a.angie.f@gmail.com>
2021-05-21 13:12:01 -0400

    update Level1 file

--- src/Level1.java
+++ src/Level1.java
@@ -6,29 +6,31 @@ public class Level1
     private static ArrayList<String> string_state = new ArrayList<String>();
     private static boolean undo_flag = false;
     private static int undo_counter = 0;
-    private static String last_action = "";
+    private static String undo_str = "";
 
     public static String BastShoe(String command) {
 
         if(command.charAt(0) == '1') {
+            if(string_state.isEmpty())
+                string_state.add("");
+
             for(int i = 2; i < command.length(); i++)
                 str += command.charAt(i);
+
             if(undo_flag == true) {
                 String temp = "";
                 undo_counter = 0;
-                if(string_state.isEmpty()) temp = "";
-                else temp = string_state.get(string_state.size() - 1);
+                temp = undo_str;
                 string_state.clear();
                 string_state.add(temp);
+                undo_flag = false;
             }
             string_state.add(str);
-            last_action = "";
-            last_action = command;
-            undo_flag = false;
+            System.out.println(string_state);
             return str;
 
         } if(command.charAt(0) == '2') {
-
+            if(string_state.isEmpty()) string_state.add("");
             String temp_str = "";
             String N = "";
             for(int i = 2; i < command.length(); i++) {
@@ -51,12 +53,14 @@ public class Level1
             str = "";
             str = temp_str;
             string_state.add(str);
-            last_action = "";
-            last_action = command;
             undo_flag = false;
+            System.out.println(string_state);
             return str;
 
         } if(command.charAt(0) == '3') {
+            if(string_state.isEmpty())
+                string_state.add("");
+
             String temp_str = "";
             if(Integer.parseInt(String.valueOf(command.charAt(2))) >= str.length()) {
                 return temp_str;
@@ -65,18 +69,24 @@ public class Level1
             return temp_str;
 
         } if(command.charAt(0) == '4') {
+            if(string_state.isEmpty())
+                string_state.add("");
+
             undo_flag = true;
-            if(string_state.isEmpty()) {
-                return str;
-            }
             undo_counter++;
             if(string_state.size() < undo_counter + 1) undo_counter--;
             str = string_state.get(string_state.size() - 1 - undo_counter);
+            System.out.println(string_state);
+            undo_str = str;
             return str;
 
         } if(command.charAt(0) == '5') {
+            if(string_state.isEmpty())
+                string_state.add("");
+
             if(undo_counter != 0) undo_counter--;
             str = string_state.get(string_state.size() - 1 - undo_counter);
+            System.out.println(string_state);
             return str;
         }
         return str;
