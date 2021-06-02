03a3de7e7ca191751bffcd7738ee9e5781fe885a
Angie Akhmatshina <a.angie.f@gmail.com>
2021-06-02 13:02:12 -0400

    update Level1 file

--- src/Level1.java
+++ src/Level1.java
@@ -2,46 +2,36 @@ import java.util.*;
 
 public class Level1
 {
-    public static String BiggerGreater(String input) {
-        char dec_element = ' ';
-        int ind = 0, dif = 0, swap_ind = 0;
-        String temp_str = "", res = "";
-        char sym1 = ' ', sym2 = ' ';
+    public static boolean SherlockValidString(String s) {
+        boolean flag = true;
+        LinkedHashMap<Character, Integer> symbols = new LinkedHashMap();
+        int num = 0;
+        for(int i = 0; i < s.length(); i++)
+            symbols.put(s.charAt(i), num);
 
-        for(int i = input.length() - 1; i > 0; i--) {
-            sym1 = input.charAt(i - 1);
-            sym2 = input.charAt(i);
-            if (sym1 <= sym2) {
-                dec_element = input.charAt(i - 1);
-                ind = i - 1;
-                break;
+        for(int i = 0; i < s.length(); i++) {
+            num = symbols.get(s.charAt(i)) + 1;
+            symbols.put(s.charAt(i), num);
+        }
+
+        ArrayList<Integer> count_arr = new ArrayList<>();
+        for(Map.Entry<Character, Integer> entry : symbols.entrySet()) {
+            count_arr.add(entry.getValue());
+        }
+
+        int counter = count_arr.get(0);
+        int temp = 0, temp_counter = 0;
+        for(int i = 0; i < count_arr.size(); i++) {
+            if(count_arr.get(i) != counter) {
+                if(temp_counter == 0) {
+                    temp = count_arr.get(i) - 1;
+                    temp_counter++;
+                    if (temp != counter && temp != 0)
+                        flag = false;
+                } else flag = false;
             }
         }
-
-        dif = input.charAt(ind + 1) - input.charAt(ind);
-        for(int j = ind; j < input.length() - 1; j++) {
-            if((input.charAt(j + 1) - dec_element) <= dif && (input.charAt(j + 1) - dec_element) > 0) {
-                dif = input.charAt(j + 1) - dec_element;
-                swap_ind = j + 1;
-            }
-        }
-        if(dif <= 0) return "";
-
-        for(int i = 0; i < input.length(); i++) {
-            if(i == ind) {
-                temp_str += input.charAt(swap_ind);
-            } else if(i == swap_ind) {
-                temp_str += input.charAt(ind);
-            } else temp_str += input.charAt(i);
-        }
-
-        for(int j = 0; j <= ind; j++)
-            res += temp_str.charAt(j);
-
-        for(int i = temp_str.length() - 1; i > ind; i--) {
-            res += temp_str.charAt(i);
-        }
-
-        return res;
+        ;
+        return flag;
     }
 }
