d6611c87a5c4c3ecb021999e49fe5250c8e16f30
Angie Akhmatshina <a.angie.f@gmail.com>
2021-06-16 12:50:54 -0400

    update Level1 file

--- src/Level1.java
+++ src/Level1.java
@@ -2,119 +2,41 @@ import java.util.*;
 
 public class Level1
 {
-    public static void MatrixTurn(String Matrix[], int M, int N, int T) {
+    public static boolean TransformTransform(int A[], int N) {
 
-        int[][] matrix = new int[M][N];
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int el:A)
+            list.add(el);
 
-        for(int i = 0; i < Matrix.length; i++)
-            for(int j = 0; j < Matrix[i].length(); j++)
-                matrix[i][j] = Integer.parseInt(String.valueOf(Matrix[i].charAt(j)));
+        ArrayList<Integer> list_B = new ArrayList<>();
+        list_B = S(list);
 
-        int numberOfLayers = Math.min(M, N) / 2;
+        ArrayList<Integer> fin_arr = new ArrayList<>();
+        fin_arr = S(list_B);
 
-        ArrayList<ArrayList<Integer>> layers = new ArrayList<>();
+        int total = 0;
+        for(int i = 0; i < fin_arr.size(); i++)
+            total += fin_arr.get(i);
 
-        for(int layer = 0; layer < numberOfLayers; layer++) {
-            ArrayList<Integer> layer_matrix = new ArrayList<>();
-            layerFormer(layer_matrix, matrix, layer);
-            layers.add(layer_matrix);
-        }
-
-        matrixCleaner(matrix);
-
-        ArrayList<ArrayList<Integer>> rotatedLayers = new ArrayList<>();
-
-        int offset = 0;
-        if(T < layers.get(0).size()) offset = T;
-        else offset = layers.get(0).size() % T;
-
-
-        for(int layer = 0; layer < layers.size(); layer++) {
-            ArrayList<Integer> el = new ArrayList<>();
-            for(int s = layers.get(layer).size() - offset; s < layers.get(layer).size(); s++) {
-                el.add(layers.get(layer).get(s));
-            }
-            for(int s = 0; s < layers.get(layer).size() - offset; s++) {
-                el.add(layers.get(layer).get(s));
-            }
-            rotatedLayers.add(el);
-        }
-
-        for(int layer = 0; layer < numberOfLayers; layer++) {
-            matrixPutter(layer, rotatedLayers, matrix);
-        }
-
-        for(int i = 0; i < Matrix.length; i++)
-            Matrix[i] = "";
-
-        for(int i = 0; i < Matrix.length; i++) {
-            for(int j = 0; j < matrix[0].length; j++)
-                Matrix[i] += matrix[i][j];
-        }
-
+        if(total % 2 == 0)
+            return true;
+        else return false;
     }
 
-
-    public static void matrixPutter(int layer, ArrayList<ArrayList<Integer>> layers, int[][] matrix) {
-        // top left to right
+    public static ArrayList<Integer> S(ArrayList<Integer> A) {
         int k = 0;
-        for(int j = layer; j < matrix[0].length - layer; j++) {
-            matrix[layer][j] = layers.get(layer).get(k);
-            k++;
+        ArrayList<Integer> B = new ArrayList<>();
+        for(int i = 0; i < A.size(); i++) {
+            for(int j = 0; j < A.size() - i; j++) {
+                k = i + j;
+                int max = A.get(j);
+                for(int s = j; s <= k; s++) {
+                    if(A.get(s) > max)
+                        max = A.get(s);
+                }
+                B.add(max);
+            }
         }
-
-
-        // right top to bottom
-        for(int i = layer + 1; i < matrix.length - layer; i++) {
-            matrix[i][matrix[0].length - 1 - layer] = layers.get(layer).get(k);
-            k++;
-        }
-
-        // bottom right to left
-        for(int j = matrix.length - layer; j > layer; j--) {
-            matrix[matrix.length - 1 - layer][j] = layers.get(layer).get(k);
-            k++;
-        }
-
-        // left bottom to top
-        for(int i = matrix.length - 1 - layer; i >= layer + 1; i--) {
-            matrix[i][layer] = layers.get(layer).get(k);
-            k++;
-        }
+        return B;
     }
-
-    public static void layerFormer(ArrayList<Integer> layer_matrix, int[][] matrix, int layer) {
-        //layer_matrix.add(matrix[layer + 1][layer]);
-
-        // top left to right
-        for(int j = layer; j < matrix[0].length - layer; j++) {
-            layer_matrix.add(matrix[layer][j]);
-        }
-        System.out.println(layer_matrix);
-
-        // right top to bottom
-        for(int i = layer + 1; i < matrix.length - layer; i++) {
-            layer_matrix.add(matrix[i][matrix[0].length - 1 - layer]);
-        }
-        System.out.println(layer_matrix);
-
-        // bottom right to left
-        for(int j = matrix.length - layer; j > layer; j--)
-            layer_matrix.add(matrix[matrix.length - 1 - layer][j]);
-        System.out.println(layer_matrix);
-
-        // left bottom to top
-        for(int i = matrix.length - 1 - layer; i >= layer + 1; i--)
-            layer_matrix.add(matrix[i][layer]);
-        System.out.println(layer_matrix);
-
-    }
-
-    public static void matrixCleaner(int[][] matrix) {
-        // Initialize matrix with zeros
-        for(int i = 0; i < matrix.length; i++)
-            for(int j = 0; j < matrix[i].length; j++)
-                matrix[i][j] = 0;
-    }
-
 }
