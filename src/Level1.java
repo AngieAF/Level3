7edaf9ca6ce938929a030f18c9ae1167a4b6884d
Angie Akhmatshina <a.angie.f@gmail.com>
2021-06-19 10:08:00 -0400

    update Level1 file

--- src/Level1.java
+++ src/Level1.java
@@ -2,41 +2,30 @@ import java.util.*;
 
 public class Level1
 {
-    public static boolean TransformTransform(int A[], int N) {
-
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int el:A)
-            list.add(el);
-
-        ArrayList<Integer> list_B = new ArrayList<>();
-        list_B = S(list);
-
-        ArrayList<Integer> fin_arr = new ArrayList<>();
-        fin_arr = S(list_B);
-
-        int total = 0;
-        for(int i = 0; i < fin_arr.size(); i++)
-            total += fin_arr.get(i);
-
-        if(total % 2 == 0)
-            return true;
-        else return false;
-    }
-
-    public static ArrayList<Integer> S(ArrayList<Integer> A) {
-        int k = 0;
-        ArrayList<Integer> B = new ArrayList<>();
-        for(int i = 0; i < A.size(); i++) {
-            for(int j = 0; j < A.size() - i; j++) {
-                k = i + j;
-                int max = A.get(j);
-                for(int s = j; s <= k; s++) {
-                    if(A.get(s) > max)
-                        max = A.get(s);
+    public static boolean white_walkers(String village) {
+        int c = 0, counter = 0;
+        char num = ' ';
+        int num_pos = 0;
+        for(int s = 0; s < village.length(); s++) {
+            if(Character.isDigit(village.charAt(s))) {
+                c++;
+                if(c > 1) {
+                    if((Integer.parseInt(String.valueOf(num)) +
+                            Integer.parseInt(String.valueOf(village.charAt(s))) == 10)) {
+                        for(int i = s - 1; i > num_pos; i--) {
+                            if(village.charAt(i) == '=')
+                                counter++;
+                        }
+                        if(counter != 3) return false;
+                        counter = 0;
+                    }
                 }
-                B.add(max);
+                num = village.charAt(s);
+                num_pos = s;
             }
+
         }
-        return B;
+        if(c  <= 1) return false;
+        return true;
     }
 }
