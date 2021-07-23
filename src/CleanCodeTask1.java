4be8ee050c3893387c586bfad978ba0b6dca68df
Angie Akhmatshina <a.angie.f@gmail.com>
2021-07-23 13:05:00 +0300

    update CleanCodeTask1 file

---
+++ src/CleanCodeTask1.java
@@ -0,0 +1,38 @@
+public class CleanCodeTask1 {
+    res – resultingMagicWord
+// волшебное слово, полученное путем преобразования обычного слова
+
+    total – keyValueSum
+// sum of the resulting value after double transformation
+
+    fin_arr – doubleTransformedArray
+// array after second transformation
+
+    res – doorsOpenClosed
+// результирующая последовательность дверей
+
+    counter – zombieCounter
+// number of zombies in the sequence
+
+    num_pos – digitPosition
+// Position of the digit in the sequence
+
+    fin_arr – transformedTreeArray
+// Resulting array that represents the tree after the transformation
+
+    symbols – initialPassword
+// the initial sequence of symbols that represent a password
+
+    total – numberOfDiamonds
+// total number of diamonds
+
+    arr – playersPositioning
+// array that stores players positioning in the field
+
+    dec_element – firstDecliningLetter
+// first letter in the string that is less than the following letter
+
+    swap_ind – indexOfElementToSwap
+// positioning of the element to swap
+
+}
