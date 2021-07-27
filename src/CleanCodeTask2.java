42d3544bd39f3380fa1f636189518d94b126c174
Angie Akhmatshina <a.angie.f@gmail.com>
2021-07-27 17:57:18 +0300

    add CleanCodeTask1 file

---
+++ src/CleanCodeTask2.java
@@ -0,0 +1,29 @@
+public class CleanCodeTask2 {
+    6.1
+    qp – qualifiedPurchaser
+    flag – isValidCode
+    fin_arr – treeOfLifeFutureModel
+    matrix – initialTreeOfLifeMatrix
+    res – finalMagicWord
+
+6.2
+    counter – equalSymbolsCount
+    arr_t – transponseMatrix
+    i – rowIndex
+    dateSent – messageSentTimestamp
+
+6.3
+    housePrice
+            pricePerRoom
+    floorsCount
+            areaInSquareFeet
+    houseFeatures
+
+6.4
+    x – volumeOfThePot
+    y – amountEatenPerDay
+    length – pyramidLength
+    cs – stringAsCharArray
+    sub_length - lengthOfSubstring
+
+}
