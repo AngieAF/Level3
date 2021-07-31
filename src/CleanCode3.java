9185ba6fa61752b6102942c99c4e1cbe7ede13e9
Angie Akhmatshina <a.angie.f@gmail.com>
2021-07-31 13:07:48 +0300

    add CleanCode3 file

---
+++ src/CleanCode3.java
@@ -0,0 +1,24 @@
+public class CleanCode3 {
+    7.1
+    res – isSortable
+    res – isFound
+    fin – isEven
+    flag – isValid
+    flag – isPassword
+
+7.2
+    When checking if it’s a file before deleting it: found
+    When searching for a substring in a string: success
+
+7.3
+    In tree of life program it’s better to use year for the name of the index instead of the typical, I, j, k. In matrix turn program it’s better to use layer name to identify the layer of the matrix.
+
+7.4
+    begin/end can be used in cycle to show the beginning/end of array/queue etc.
+    first/last could be used in matrix turn program to count the layers of the matrix.
+
+7.5
+
+    When copying elements in the array I could use more explicit name like keySymbol instead of temp, numberOfValidSymbols instead temp_counter.
+
+}
