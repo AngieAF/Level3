61772fa65487a93eef7cde4cbfc00f756ef8f591
Angie Akhmatshina <a.angie.f@gmail.com>
2021-08-04 21:01:16 +0300

    add CleanCodeTask4 file

---
+++ src/CleanCodeTask4.java
@@ -0,0 +1,14 @@
+public class CleanCodeTask4 {
+    total – numDiamonds
+    sym1 – beforeSymbol
+    sym2 – afterSymbol
+    ext1 – imageExtensionToConvertFrom
+    ext2 – image ExtensionToConvertTo
+    x – xCoordinate
+    y – yCoordinate
+    w – imageWidth
+    h – imageHeight
+    num – isDigit
+    num_pos – digitId
+    counter - numWords
+}
