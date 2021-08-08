cfc2114fcbffeccd15b9cfb9787f27f34e804745
Angie Akhmatshina <a.angie.f@gmail.com>
2021-08-08 22:51:14 +0300

    add CleanCodeTask5 file

---
+++ src/CleanCodeTask5.java
@@ -0,0 +1,20 @@
+public class CleanCodeTask5 {
+
+3.1
+    Appliance and HomeAppliance, Appliance should be renamed to Device to avoid mixup of two classes.
+    FinInstrument – FinancialInstrument
+    Account – TradingAccount
+    Map – gameMap
+    User1 – vkontakteUser
+
+3.2
+    add_funds – addFunds
+    set_trading – setTrading
+    set_margin – setMargin
+    authenticate – authenticateAccount
+    qualified_client – setQualifiedClient
+    minCalculator – calculateMinimum
+    arFinder – findSymbol
+
+}
+
