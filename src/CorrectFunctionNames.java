762e52fb6f1e22ccc0663985ac7f73978708e8d3
Angie Akhmatshina <a.angie.f@gmail.com>
2021-08-12 13:04:26 +0300

    add CorrectFunctionNames file

---
+++ src/CorrectFunctionNames.java
@@ -0,0 +1,26 @@
+public class CorrectFunctionNames {
+    matrixPrinter – printMatrix - нужно использовать глаголы в именах функций
+
+    matrixCleaner – setMatrixToZeros - нужно использовать глаголы в именах функций
+
+    Arranger – isSorted - более понятное имя функции
+
+    layerFormer – formMatrixLayers - нужно использовать глаголы в именах функций
+
+    converter – convertImageFormat - более детальное имя функции
+
+    s – applySTransformation - более понятное имя функции
+
+    matrixPrinter – printTreeOfLife - более понятное имя функции
+
+    arFinder – findSymbol - более понятное имя функции
+
+    remover – deleteDirectory - более понятное имя функции
+
+    listFiles – listDirectoryFiles - более детальное имя функции
+
+    calculator – sumFilesContent - более понятное имя функции
+
+    minCalc – findArrayMinimum - более понятное имя функции
+
+}
