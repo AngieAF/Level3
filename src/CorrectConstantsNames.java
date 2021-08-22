b4993a063d1b14493a771357fa78a7612f665f56
Angie Akhmatshina <a.angie.f@gmail.com>
2021-08-22 09:02:29 +0300

    add CorrectConstantsNames file

---
+++ src/CorrectConstantsNames.java
@@ -0,0 +1,15 @@
+public class CorrectConstantsNames {
+    arr.length - PLAYER_POSITION_COUNT - instead of calculating the length of the array, we'll use a constant
+    w - IMAGE_WIDTH - rename constant to be all capital letters
+    h - IMAGE_HEIGHT - rename constant to be all capital letters
+    k - STEPS_COUNT - rename to give more meaningful name
+            doors.length - DOORS_COUNT - instead of calculating the length of the array, we'll use a constant
+    matrix.length - MATRIX_ROW_COUNT - instead of calculating the length of the array, we'll use a constant
+    matrix[i].length - MATRIX_COLUMN_COUNT - instead of calculating the length of the array, we'll use a constant
+    numberOfLayers - LAYER_COUNT - rename constant to be all capital letters
+    s.length - PASSWORD_LENGTH - rename to give more meaningful name
+            A.size - TRANSFORMATION_A_SIZE - rename constant to be all capital letters
+    N - YEAR_COUNT - rename to give more meaningful name
+            village.length - WALKERS_COUNT - rename to give more meaningful name
+
+}
