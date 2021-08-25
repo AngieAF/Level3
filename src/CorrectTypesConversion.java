bd7e1e155c7a87afb9b6af3d80432fc6f33dd40a
Angie Akhmatshina <a.angie.f@gmail.com>
2021-08-25 15:48:05 -0400

    add CorrectTypesConversion file

---
+++ src/CorrectTypesConversion.java
@@ -0,0 +1,28 @@
+public class CorrectTypesConversion {
+    (Integer.parseInt(String.valueOf(num)) + Integer.parseInt(String.valueOf(village.charAt(s))) == 10) ->
+    boolean sleepwalkerDetected = (beginningCitizenValue + endingCitizenValue == TEMPERATURE_DECREASE);
+
+    total % 2 == 0 -> if(isTotalEven);
+
+    temp != counter && temp != 0 -> if(!isPassword);
+
+    file.isFile() && file.getName().contains(ext) -> isCorrectFile
+
+    if(doors[j] == 0) -> isClosedDoor
+
+    tree[i].charAt(j) == '+' -> isBranch
+
+    sym1 <= sym2 -> isGreaterElement;
+
+    (input.charAt(j + 1) - dec_element) <= dif && (input.charAt(j + 1) - dec_element) > 0 ->
+        isShorterLexicographicWord;
+
+     if(Arranger(arr)) -> if(isSortableInOneStep);
+
+     if(price < min_price) -> isCheaper;
+
+     if(last_digit != digit_to_remove) - > !isDigitToRemove;
+
+     if(grade % 3 == 0) -> isGradeThree;
+
+}
