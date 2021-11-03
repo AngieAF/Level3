d5286076ffd6fbb3e8acf014046e666419118755
Angie Akhmatshina <a.angie.f@gmail.com>
2021-11-03 17:27:39 -0400

    update DynArray

--- src/DynArray.java
+++ src/DynArray.java
@@ -17,7 +17,7 @@ public class DynArray<T>
     public void makeArray(int new_capacity)
     {
         T [] temp_array = (T[]) Array.newInstance(this.clazz, new_capacity);
-        for (int i = 0; i < count - 1; i++) {
+        for (int i = 0; i <= count - 1; i++) {
             temp_array[i] = array[i];
         }
         array = temp_array;
@@ -26,12 +26,12 @@ public class DynArray<T>
 
     public T getItem(int index)
     {
-        if (index >= 0 && index <= array.length - 1) {
+        try {
             return array[index];
-        } else {
-            return null; // add exception here
         }
-
+         catch (IndexOutOfBoundsException e) {
+         }
+        return null;
     }
 
     public void append(T itm)
@@ -47,43 +47,87 @@ public class DynArray<T>
 
     public void insert(T itm, int index)
     {
-        if (index >= 0 && index <= array.length - 1) {
-            if (count == array.length) {
-                makeArray(2 * array.length);
-            }
+        if (index == count) {
+            append(itm);
+        } else {
+            try {
+                if (count == array.length) {
+                    makeArray(2 * array.length);
+                }
 
-            for (int i = count - 1; i >= index; i--) {
-                array[i + 1] = array[i];
-            }
+                for (int i = count - 1; i >= index; i--) {
+                    array[i + 1] = array[i];
+                }
 
-            array[index] = itm;
-            count++;
+                array[index] = itm;
+                count++;
+            } catch (IndexOutOfBoundsException e) {
+                //System.out.println(e);
+            }
         }
+
     }
 
     public void remove(int index)
     {
-        if (count > 0) {
-            for (int i = index; i <= count - 1; i++) {
-                array[i] = array[i + 1];
-            }
-            count--;
-        }
+        try {
+            if(index >= 0 && index <= count - 1) {
+                if (count > 0) {
+                    for (int i = index; i < count - 1; i++) {
+                        array[i] = array[i + 1];
+                    }
+                    array[count - 1] = null;
+                    count--;
 
-        if ((count * 3) / 2 > 16) {
-            makeArray(16);
+                    int new_capacity = 0;
+                    if (count <= (capacity / 2)) {
+                        new_capacity = capacity * 2 / 3;
+                        if (new_capacity < 16) {
+                            new_capacity = 16;
+                        }
+                        makeArray(new_capacity);
+                    }
+                }
+
+            }
+        } catch (IndexOutOfBoundsException e) {
+
         }
+    }
+
+    public String arrayPrint() {
+        String str = "";
+        for (int i = 0; i < array.length; i++) {
+            str += array[i];
+            if (i < array.length - 1) {
+                str += ", ";
+            }
+        }
+        return "[" + str + "]";
     }
 
     /*public static void main(String[] args) {
         DynArray dynArr = new DynArray<Integer>(Integer.class);
-        System.out.println(dynArr.capacity);
+        int itm = 30;
+        dynArr.append(0);
+        dynArr.append(1);
         dynArr.append(2);
+        dynArr.append(3);
+        dynArr.append(4);
+        dynArr.append(5);
+        dynArr.append(6);
+        dynArr.append(7);
+        dynArr.append(8);
+        dynArr.append(9);
         dynArr.append(10);
+        dynArr.append(11);
+        dynArr.append(12);
+        dynArr.append(13);
+        dynArr.append(14);
+        dynArr.append(15);
 
-        dynArr.remove(1);
-        System.out.println(dynArr.getItem(0));
+        dynArr.insert(itm, 40);
 
-    }*/
+    } */
 
 }
