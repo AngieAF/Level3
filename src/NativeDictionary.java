90f35fb8a896c3f407d7422bc5f9980c07e815a1
Angie Akhmatshina <a.angie.f@gmail.com>
2021-12-09 14:30:41 -0500

    update NativeDictionary.java

--- src/NativeDictionary.java
+++ src/NativeDictionary.java
@@ -23,25 +23,68 @@ class NativeDictionary<T>
 
         for (byte b : bytes) {
             sum += b;
-            System.out.println(sum);
+            //System.out.println("Sum " + sum);
         }
 
+        //System.out.println((sum % size));
         return sum % size;
+    }
+
+    public int seekSlot(String value)
+    {
+        // находит индекс пустого слота для значения, или -1
+
+        int slotId = hashFun(value);
+        int round = 1;
+        int step = 3;
+
+        while (counter <= size) {
+            step = step * round;
+            if (step >= size) {
+                step = 1;
+            }
+
+            for (int i = slotId; i < slots.length; i += step) {
+                if (slots[i] == null) {
+                    counter++;
+                    return i;
+                }
+                if (slots[i] == value) {
+                    return i;
+                }
+            }
+
+            for (int i = 0; i < slotId; i += step) {
+                if (slots[i] == null) {
+                    counter++;
+                    return i;
+                }
+                if (slots[i] == value) {
+                    return i;
+                }
+            }
+            round++;
+        }
+        return -1;
     }
 
     public boolean isKey(String key)
     {
         // возвращает true если ключ имеется,
         // иначе false
-
-        return slots[hashFun(key)] != null;
+        for (int i = 0; i < slots.length; i++) {
+            if (slots[i] == key) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void put(String key, T value)
     {
         // гарантированно записываем
         // значение value по ключу key
-        int hashId = hashFun(key);
+        int hashId = seekSlot(key);
         if (hashId == -1) {
             throw new RuntimeException("Not enough space");
         }
@@ -56,18 +99,21 @@ class NativeDictionary<T>
         // или null если ключ не найден
 
         int hashId = hashFun(key);
-        if (hashId == -1) {
-            return null;
+        if (isKey(key)) {
+            return values[hashId];
         }
-        return values[hashId];
+        return null;
     }
 
+
+
     /*public static void main(String[] args) {
-        NativeDictionary<Integer> nativeDictionary = new NativeDictionary<>(5, Integer.class);
+        NativeDictionary<Integer> nativeDictionary = new NativeDictionary<>(1000, Integer.class);
         nativeDictionary.put("Line 1", 1);
         nativeDictionary.put("Line 2", 2);
-        System.out.println(nativeDictionary.isKey("Line 1"));
+        System.out.println(nativeDictionary.isKey("Line 4"));
         System.out.println(nativeDictionary.get("Line 35"));
 
+        System.out.println(nativeDictionary);
     }*/
 }
