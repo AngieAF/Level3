5978e58c1c7d1d583699756ac2d4ae26c424c8a9
Angie Akhmatshina <a.angie.f@gmail.com>
2022-03-16 11:40:06 -0400

    Add printEvenValues.java

---
+++ src/printEvenValues.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class printEvenValues {
+    private void printEvenValues(LinkedList<Integer> list) {
+        if (list.isEmpty()) {
+            return;
+        }
+        LinkedList listCopy = new LinkedList(list);
+        if ((list.get(0) % 2) == 0) {
+            System.out.println(listCopy.get(0));
+        }
+        listCopy.remove(0);
+        printEvenValues(listCopy);
+    }
+}
