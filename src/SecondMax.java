9aabba28f74857ca72d0e1af7bcea29801b60cf5
Angie Akhmatshina <a.angie.f@gmail.com>
2022-03-30 10:35:53 -0400

    Add SecondMax.java

--- src/SecondMax.java
+++ src/SecondMax.java
@@ -1,32 +1,25 @@
 import java.util.LinkedList;
 
 public class SecondMax {
-    private int secondMaxElement(LinkedList<Integer> list) {
+    public int secondMaxElement(LinkedList<Integer> list) {
         LinkedList<Integer> listCopy = new LinkedList<>(list);
-        int max = getMaxRec(listCopy, list.get(0));
-        boolean isMax2Empty = true;
 
-        int max2 = 0;
-        for (int i = 0; i < list.size(); i++) {
-            if (isMax2Empty) {
-                if (list.get(i) != max) {
-                    max2 = list.get(i);
-                    isMax2Empty = false;
-                }
-            } else {
-                if (list.get(i) > max2 && list.get(i) != max) {
-                    max2 = list.get(i);
-                }
-            }
-        }
-        return max2;
+        return getMaxRec(listCopy, 0, Integer.MIN_VALUE, Integer.MIN_VALUE + 1);
     }
 
-    private int getMaxRec(LinkedList<Integer> list, int max) {
-        if (list.isEmpty()) {
-            return max;
+    private int getMaxRec(LinkedList<Integer> list, int index, int largest, int secondLargest) {
+        if (index == list.size()) {
+            return secondLargest;
         }
-        int value = list.remove(0);
-        return getMaxRec(list, Math.max(value, max));
+        int element = list.get(index);
+
+        if (element > secondLargest) {
+            if(element > largest) {
+                return getMaxRec(list, index + 1, element, largest);
+            } if (element < largest) {
+                return getMaxRec(list, index + 1, largest, element);
+            }
+        }
+        return getMaxRec(list, index + 1, largest, secondLargest);
     }
 }
